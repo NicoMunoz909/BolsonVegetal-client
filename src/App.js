3013bcc81e8f99f4e03ae9a97d7146b5da955132
NicoMunoz909 <nicolas.munoz@outlook.es>
2023-01-09 17:46:12 -0300

    Create banner footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import BannerCarousel from './Components/BannerCarousel';
+import BannerFooter from './Components/BannerFooter';
 import Navbar from './Components/Navbar';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Navbar />
       <BannerCarousel />
+      <BannerFooter />
     </div>
   );
 }
