ff0cb9b650090d1102c0d7042d919db34fad15f4
NicoMunoz909 <nicolas.munoz@outlook.es>
2023-01-22 18:14:33 -0300

    Create shopping page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
+import ShoppingPage from './Components/ShoppingPage';
 
 function App() {
+
+  const products = [{name: 'Naranja de jugo', price: '250', priceType: 'KG'}, {name: 'Banana boliviana', price: '320', priceType: 'KG'}, {name: 'Palta Hass', price: '300', priceType: 'U'}, {name: 'Champignon', price: '250', priceType: '100G'}]
+
   return (
     <div className="App">
       <Navbar />
       <div style={{marginTop: '65px'}}>
-        <Home />
+        {false && <Home />}
+        <ShoppingPage titulo='Frutas' filtros={['Agroecologico', 'Citricos']} products={products}/>
       </div>
       <Footer />
     </div>
---
+++ src/Components/ShoppingPage/index.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { FaChevronDown, FaSearch } from 'react-icons/fa'
+import banner from '../../Assets/frutasBanner.jpg'
+import Item from '../../Assets/item.jpg'
+import './shoppingPage.css'
+
+const ShoppingPage = ({titulo, filtros, products}) => {
+
+  const filterOptions = filtros
+
+  function toggleFilters() {
+    document.getElementsByClassName('filterOptions')[0].classList.toggle('hmenu-visible')
+    document.getElementById('filtersArrow').classList.toggle('rotated')
+  }
+
+  return (
+    <div>
+      <div className='imgContainer shoppingPageBanner' id='imgContainer'>
+        <h2>{titulo}</h2>
+        <img src={banner} alt="" />
+      </div>
+      <div className='filtersBar'>
+        <div className='searchBar alignCenter'>
+          <FaSearch />
+          <input type="text" />
+        </div>
+        <div>
+          <div className='alignCenter' onClick={toggleFilters}>
+            Filtros
+            <FaChevronDown style={{alignSelf: 'flex-end'}} id='filtersArrow'/>
+          </div>
+          <div className='filterOptions'>
+            {filterOptions.map( i => {
+              return (
+                <div>
+                  <input type="checkbox" id={i} name={i} value={i}/>
+                  <label htmlFor={i}>{i}</label>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+      <div className='itemsCarouselContainer'>
+        <div className='itemsContainer' style={{flexWrap: 'wrap'}}>
+          {products.map( i => {
+            return (
+              <div className='itemContainer' id='items-container' style={{width: '40%'}}>
+                <div className='item'>
+                  <img src={Item} alt={i.name} />
+                  <h3>{i.name}</h3>
+                  <p>${i.price} x {i.priceType}</p>
+                  <button>Añadir al carrito</button>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ShoppingPage
