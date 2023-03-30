9243d6304309b1a3e33f20276a045c2ea4d98371
NicoMunoz909 <nimunoz28@gmail.com>
2023-03-30 11:58:01 -0500

    Add subscribe submit handler

--- src/Components/SubscribeBanner/index.js
+++ src/Components/SubscribeBanner/index.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import banner from '../../Assets/subscribe-banner.jpg'
 import './subscribeBanner.css'
 
 const SubscribeBanner = () => {
+
+  const [name, setName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('')
+
+  const encodedText =  `Hola! Quisiera suscribirme a la lista de difusion.
+Nombre: ${name}
+Telefono: ${phoneNumber}
+Muchas Gracias!`
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    window.open(`https://wa.me/543413062939?text=${encodeURIComponent(encodedText)}`)
+    e.target.reset()
+  }
+
   return (
     <div className='subBanner'>
-      <form>
+      <form onSubmit={handleSubmit}>
         <p>Suscribite a nuestra lista de difusion</p>
         <label htmlFor="nombre"></label>
-        <input type="text" placeholder='Nombre'/>
+        <input onChange={(e) => setName(e.target.value)} type="text" placeholder='Nombre y apellido' required/>
         <label htmlFor="telefono"></label>
-        <input type="phone" placeholder='Telefono'/>
-        <button>Suscribirme!</button>
+        <input onChange={(e) => setPhoneNumber(e.target.value)} type="phone" placeholder='Teléfono' required/>
+        <button type='submit'>Suscribirme!</button>
       </form>
       <div className='imgContainer' id='imgContainer'>
         <img src={banner} alt="" />
