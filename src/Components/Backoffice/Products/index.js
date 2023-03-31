b12cc5032379af478e40163dcf36bf574b41f176
NicoMunoz909 <nimunoz28@gmail.com>
2023-03-31 12:46:53 -0500

    Modify editing modal to use for adding. Add search bar and add button to catalog manager

--- src/Components/Backoffice/Products/index.js
+++ src/Components/Backoffice/Products/index.js
@@ -1,6 +1,6 @@
 import './Products.css'
 import * as data from '../../../data'
-import { FaEdit, FaTrashAlt } from 'react-icons/fa'
+import { FaEdit, FaTrashAlt, FaSearch, FaPlus } from 'react-icons/fa'
 import itemImg from '../../../Assets/item.jpg'
 import { useState } from 'react'
 import EditingModal from './EditingModal'
@@ -9,8 +9,15 @@ import DeleteConfirmModal from './DeleteConfirmModal'
 const Products = () => {
 
   const [editing, setEditing] = useState(false)
+  const [adding, setAdding] = useState(false)
   const [deleteConfirm, setDeleteConfirm] = useState(false)
   const [selectedItem, setSelectedItem] = useState(undefined)
+  const [searchText, setSearchText] = useState('')
+
+  const handleAdd = (e) => {
+    e.preventDefault()
+    setAdding(false)
+  }
 
   const handleEdit = (e) => {
     e.preventDefault()
@@ -24,6 +31,7 @@ const Products = () => {
 
   const handleCancel = (e) => {
     e.preventDefault()
+    setAdding(false)
     setEditing(false)
     setDeleteConfirm(false)
   }
@@ -32,14 +40,24 @@ const Products = () => {
     e.target.classList.toggle('switchInner__off')
   }
   
-  const productsArray = [data.Almacen, data.Bebidas, data.Bolsones, data.Congelados, data.Dietetica, data.Frutas, data.Verduras, data.Ofertas]
+  const productsArray = [data.Almacen, data.Bebidas, data.Bolsones, data.Congelados, data.Dietetica, data.Frutas, data.Verduras]
 
   return (
     <div>
+      {adding && <EditingModal onConfirm={handleAdd} onCancel={handleCancel}/>}
       {editing && <EditingModal item={selectedItem} onConfirm={handleEdit} onCancel={handleCancel}/>}
       {deleteConfirm && <DeleteConfirmModal item={selectedItem} onConfirm={handleDelete} onCancel={handleCancel}/>}
       <h1 className='backoffice-header'>Gestor de catálogo</h1>
-      {productsArray.map(category => category.map(item => {
+      <div className='backoffice-header'>
+        <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+          <div className='searchBar alignCenter'>
+            <FaSearch />
+            <input onChange={(e) => setSearchText(e.target.value)} style={{backgroundColor: 'transparent', border: 'none'}} type="text" /*onChange={(e) => setSearch(e.target.value)}*/ />
+          </div>
+          <FaPlus onClick={() => setAdding(true)} className='add-btn' style={{fontSize: '30px'}} />
+        </div>
+      </div>
+      {productsArray.map(category => category.filter(product => product.name.toLowerCase().includes(searchText.toLowerCase())).map(item => {
         return (
         <div className='products-itemContainer'>
           <div className="products-itemImgContainer"><img src={itemImg} alt="" /></div>
