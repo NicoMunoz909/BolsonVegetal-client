bb270f4215bdfa5710d2c862b67f6e56f6007b1e
NicoMunoz909 <nimunoz28@gmail.com>
2023-07-21 13:06:56 -0500

    Make backoffice fetch from database instead of using mock js data

--- src/Components/Backoffice/Products/index.js
+++ src/Components/Backoffice/Products/index.js
@@ -1,71 +1,115 @@
-import './Products.css'
-import * as data from '../../../data'
-import { FaEdit, FaTrashAlt } from 'react-icons/fa'
-import itemImg from '../../../Assets/item.jpg'
-import { useState } from 'react'
-import EditingModal from './EditingModal'
-import DeleteConfirmModal from './DeleteConfirmModal'
+import "./Products.css";
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
+import itemImg from "../../../Assets/item.jpg";
+import { useEffect, useState } from "react";
+import EditingModal from "./EditingModal";
+import DeleteConfirmModal from "./DeleteConfirmModal";
+import { config } from "../../../Constants";
 
 const Products = () => {
+  const [editing, setEditing] = useState(false);
+  const [deleteConfirm, setDeleteConfirm] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(undefined);
+  const [items, setItems] = useState([]);
+  const URL = config.url;
 
-  const [editing, setEditing] = useState(false)
-  const [deleteConfirm, setDeleteConfirm] = useState(false)
-  const [selectedItem, setSelectedItem] = useState(undefined)
+  useEffect(() => {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  }, [URL]);
 
   const handleEdit = (e) => {
-    e.preventDefault()
-    setEditing(false)
-  }
+    e.preventDefault();
+    setEditing(false);
+  };
 
   const handleDelete = (e) => {
-    e.preventDefault()
-    setDeleteConfirm(false)
-  }
+    e.preventDefault();
+    setDeleteConfirm(false);
+  };
 
   const handleCancel = (e) => {
-    e.preventDefault()
-    setEditing(false)
-    setDeleteConfirm(false)
-  }
+    e.preventDefault();
+    setEditing(false);
+    setDeleteConfirm(false);
+  };
 
-  const handleStockSwitch = (e,item) => {
-    e.target.classList.toggle('switchInner__off')
-  }
-  
-  const productsArray = [data.Almacen, data.Bebidas, data.Bolsones, data.Congelados, data.Dietetica, data.Frutas, data.Verduras, data.Ofertas]
+  const handleStockSwitch = (e, item) => {
+    e.target.classList.toggle("switchInner__off");
+  };
 
   return (
     <div>
-      {editing && <EditingModal item={selectedItem} onConfirm={handleEdit} onCancel={handleCancel}/>}
-      {deleteConfirm && <DeleteConfirmModal item={selectedItem} onConfirm={handleDelete} onCancel={handleCancel}/>}
-      <h1 className='backoffice-header'>Gestor de catálogo</h1>
-      {productsArray.map(category => category.map(item => {
+      {editing && (
+        <EditingModal
+          item={selectedItem}
+          onConfirm={handleEdit}
+          onCancel={handleCancel}
+        />
+      )}
+      {deleteConfirm && (
+        <DeleteConfirmModal
+          item={selectedItem}
+          onConfirm={handleDelete}
+          onCancel={handleCancel}
+        />
+      )}
+      <h1 className="backoffice-header">Gestor de catálogo</h1>
+      {items.map((item) => {
         return (
-        <div className='products-itemContainer'>
-          <div className="products-itemImgContainer"><img src={itemImg} alt="" /></div>
-          <p>{item.name}</p>
-          <p>Precio: {item.price} X {item.priceType}</p>
-          <div className="products-itemActions">
-            <div>
-              <button onClick={() => {setSelectedItem(item); setEditing(true)}}>
-                <FaEdit />
-              </button>
-              <button onClick={() => {setSelectedItem(item); setDeleteConfirm(true)}}>
-                <FaTrashAlt />
-              </button>
+          <div className="products-itemContainer">
+            <div className="products-itemImgContainer">
+              <img src={itemImg} alt="" />
             </div>
-            <div className='switchContainer'>
-              <div className='switchOutter'>
-                <div className='switchInner' onClick={(e) => handleStockSwitch(e,item)}></div>
+            <p>{item.name}</p>
+            <p>
+              Precio: {item.price} X {item.priceType}
+            </p>
+            <div className="products-itemActions">
+              <div>
+                <button
+                  onClick={() => {
+                    setSelectedItem(item);
+                    setEditing(true);
+                  }}
+                >
+                  <FaEdit />
+                </button>
+                <button
+                  onClick={() => {
+                    setSelectedItem(item);
+                    setDeleteConfirm(true);
+                  }}
+                >
+                  <FaTrashAlt />
+                </button>
               </div>
-              <p style={{width:'50px' ,margin: '0px', fontSize: '14px', textAlign: 'center', textTransform: 'uppercase'}}>stock</p>
+              <div className="switchContainer">
+                <div className="switchOutter">
+                  <div
+                    className="switchInner"
+                    onClick={(e) => handleStockSwitch(e, item)}
+                  ></div>
+                </div>
+                <p
+                  style={{
+                    width: "50px",
+                    margin: "0px",
+                    fontSize: "14px",
+                    textAlign: "center",
+                    textTransform: "uppercase",
+                  }}
+                >
+                  stock
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      )
-      }))}
+        );
+      })}
     </div>
-    )
-  }
-  
-  export default Products
+  );
+};
+
+export default Products;
