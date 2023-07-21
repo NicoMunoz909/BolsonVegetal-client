0bc722d0de8b3ebe128d783dcba458dcb407b265
NicoMunoz909 <nimunoz28@gmail.com>
2023-07-21 13:21:50 -0500

    Uncomment strict mode

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import './normalize.css'
-import Router from './Router';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./normalize.css";
+import Router from "./Router";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <Router />
-  // </React.StrictMode>
+  </React.StrictMode>
 );
-
