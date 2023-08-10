4cd665b26f708519ea3df60ea9836a8037060195
NicoMunoz909 <nimunoz28@gmail.com>
2023-08-10 16:25:05 -0500

    Change sessionStorage for localStorage

--- src/Contexts/CartContext.js
+++ src/Contexts/CartContext.js
@@ -5,42 +5,44 @@ export const CartContext = createContext(null);
 export const CartDispacthContext = createContext(null);
 
 export function CartProvider({ children }) {
-  const [cart, dispatch] = useReducer( cartReducer, (sessionStorage.getItem('cart') ? JSON.parse(sessionStorage.getItem('cart')) : []));
-  
+  const [cart, dispatch] = useReducer(
+    cartReducer,
+    localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : []
+  );
+
   return (
     <CartContext.Provider value={cart}>
-      <CartDispacthContext.Provider value={dispatch}>
-        {children}
-      </CartDispacthContext.Provider>
+      <CartDispacthContext.Provider value={dispatch}>{children}</CartDispacthContext.Provider>
     </CartContext.Provider>
-  )
+  );
 }
 
-function cartReducer (cart, action) {
+function cartReducer(cart, action) {
   switch (action.type) {
-    case 'ADD': {
-      const cartIndex = cart.findIndex(product => product.name === action.item.name)
-      if(cartIndex >= 0) {
+    case "ADD": {
+      const cartIndex = cart.findIndex((product) => product.name === action.item.name);
+      if (cartIndex >= 0) {
+        console.log(cart[cartIndex].quant);
         cart[cartIndex].quant += 1;
-        sessionStorage.setItem('cart', JSON.stringify(cart))
-        return [...cart]
+        localStorage.setItem("cart", JSON.stringify(cart));
+        return [...cart];
       }
-      sessionStorage.setItem('cart', JSON.stringify([...cart, {...action.item, quant: 1}]))
-      return [...cart, {...action.item, quant: 1}]
+      localStorage.setItem("cart", JSON.stringify([...cart, { ...action.item, quant: 1 }]));
+      return [...cart, { ...action.item, quant: 1 }];
     }
-    case 'REMOVE': {
-      const cartIndex = cart.findIndex(product => product.name === action.item.name)
+    case "REMOVE": {
+      const cartIndex = cart.findIndex((product) => product.name === action.item.name);
       if (cart[cartIndex].quant > 1) cart[cartIndex].quant -= 1;
-        sessionStorage.setItem('cart', JSON.stringify(cart))
-        return [...cart]
+      localStorage.setItem("cart", JSON.stringify(cart));
+      return [...cart];
     }
-    case 'DELETE': {
-      sessionStorage.setItem('cart', JSON.stringify(cart.filter(product => product.name !== action.name)))
-      return cart.filter(product => product.name !== action.name)
+    case "DELETE": {
+      localStorage.setItem("cart", JSON.stringify(cart.filter((product) => product.name !== action.name)));
+      return cart.filter((product) => product.name !== action.name);
     }
-    case 'CLEAR': {
-      sessionStorage.setItem('cart', []);
-      return []
+    case "CLEAR": {
+      localStorage.setItem("cart", []);
+      return [];
     }
     default:
       break;
